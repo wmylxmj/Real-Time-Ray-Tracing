595b96064e2920fbab684c2e2a7d5bfe017b57e8
wmylxmj <1397322329@qq.com>
2023-12-14 21:07:36 +0800

    20231214

---
+++ src/mesh.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wmy on 2023/12/14.
+//
+
+#include "mesh.h"
---
+++ src/mesh.h
@@ -0,0 +1,8 @@
+//
+// Created by wmy on 2023/12/14.
+//
+
+#ifndef RTRT_MESH_H
+#define RTRT_MESH_H
+
+#endif //RTRT_MESH_H
